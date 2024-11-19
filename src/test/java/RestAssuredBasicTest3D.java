78ec87b5983e1df7a549d910acb3264e20f66af1
Manyatha <manyathap@testvagrant.com>
2024-11-19 12:39:51 +0530

    Feat: Testing various log() methods

--- src/test/java/RestAssuredBasicTest3D.java
+++ src/test/java/RestAssuredBasicTest3D.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class RestAssuredBasicTest3D {
+//log() inside then() method
+    @Test
+    public void testLogging() {
+        given()
+
+                .when()
+                .get("https://reqres.in/api/users?page=3")
+
+                .then()
+                //.log().body();
+                //.log().cookies();
+                //.log().headers();
+                .log().all();
+
+    }
 }
